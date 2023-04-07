dc2c4dd9b5d4b41d968260a0d44449297913a055
sivakusi12 <sivakusi12@gmail.com>
2023-04-07 18:00:09 +0530

    Added more features

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -6,51 +6,48 @@ import { LockContext } from './LockContext';
 const UserContext = createContext();
 
 const UserContextProvider = props => {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState('');
-  const [loginStatus, setLoginStatus] = useState(false);
-  const navigate = useNavigate();
-  // const { resetRestaurantDetails } = useContext(RestaurantContext);
-  const { setLock } = useContext(LockContext);
+	const [user, setUser] = useState(null);
+	const [token, setToken] = useState('');
+	const [loginStatus, setLoginStatus] = useState(false);
+	const navigate = useNavigate();
+	const { setLock } = useContext(LockContext);
 
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('user'));
-    const key = localStorage.getItem('token');
-    if (data && key) {
-      setUser(data);
-      setToken(key);
-      setLoginStatus(true);
-    } else {
-      setUser(null);
-      setToken(null);
-    }
-  }, []);
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('user'));
+		const key = localStorage.getItem('token');
+		if (data && key) {
+			setUser(data);
+			setToken(key);
+			setLoginStatus(true);
+		} else {
+			setUser(null);
+			setToken(null);
+			setLoginStatus(false);
+		}
+	}, []);
 
-  useEffect(() => {
-    if (user && token && user !== '{}') setLoginStatus(true);
-    else setLoginStatus(false);
-  }, [user, token]);
+	useEffect(() => {
+		if (user && token && user !== '{}') setLoginStatus(true);
+		else setLoginStatus(false);
+	}, [user, token]);
 
-  const registerUser = (data, _token) => {
-    localStorage.setItem('user', JSON.stringify(data));
-    setUser(data);
-    localStorage.setItem('token', _token);
-    setToken(_token);
-  };
+	const registerUser = (data, _token) => {
+		localStorage.setItem('user', JSON.stringify(data));
+		setUser(data);
+		localStorage.setItem('token', _token);
+		setToken(_token);
+	};
 
-  const unRegisterUser = () => {
-    // resetRestaurantDetails();
-    // token && userLogout();
+	const unRegisterUser = () => {
+		localStorage.setItem('user', '{}');
+		setUser(null);
+		localStorage.setItem('token', '');
+		setLock(false);
+		setToken('');
+		navigate('/');
+	};
 
-    localStorage.setItem('user', '{}');
-    setUser(null);
-    localStorage.setItem('token', '');
-    setLock(false);
-    setToken('');
-    navigate('/');
-  };
-
-  return <UserContext.Provider value={{ user, registerUser, token, loginStatus, unRegisterUser }}>{props.children}</UserContext.Provider>;
+	return <UserContext.Provider value={{ user, registerUser, token, loginStatus, unRegisterUser }}>{props.children}</UserContext.Provider>;
 };
 
 export { UserContext, UserContextProvider };
