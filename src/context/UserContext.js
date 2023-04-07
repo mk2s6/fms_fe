0db7c50e8f394fbab808532751c13c4360f342d2
sivakusi12 <sivakusi12@gmail.com>
2023-04-07 21:41:04 +0530

    Added more features

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,53 +1,42 @@
 import { useEffect } from 'react';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LockContext } from './LockContext';
 
 const UserContext = createContext();
 
 const UserContextProvider = props => {
-	const [user, setUser] = useState(null);
 	const [token, setToken] = useState('');
 	const [loginStatus, setLoginStatus] = useState(false);
 	const navigate = useNavigate();
-	const { setLock } = useContext(LockContext);
 
 	useEffect(() => {
-		const data = JSON.parse(localStorage.getItem('user'));
 		const key = localStorage.getItem('token');
-		if (data && key) {
-			setUser(data);
+		if (key) {
 			setToken(key);
 			setLoginStatus(true);
 		} else {
-			setUser(null);
 			setToken(null);
 			setLoginStatus(false);
 		}
 	}, []);
 
 	useEffect(() => {
-		if (user && token && user !== '{}') setLoginStatus(true);
+		if (token) setLoginStatus(true);
 		else setLoginStatus(false);
-	}, [user, token]);
+	}, [token]);
 
 	const registerUser = (data, _token) => {
-		localStorage.setItem('user', JSON.stringify(data));
-		setUser(data);
 		localStorage.setItem('token', _token);
 		setToken(_token);
 	};
 
 	const unRegisterUser = () => {
-		localStorage.setItem('user', '{}');
-		setUser(null);
-		localStorage.setItem('token', '');
-		setLock(false);
+		localStorage.removeItem('token');
 		setToken('');
-		navigate('/');
+		navigate('/signin');
 	};
 
-	return <UserContext.Provider value={{ user, registerUser, token, loginStatus, unRegisterUser }}>{props.children}</UserContext.Provider>;
+	return <UserContext.Provider value={{ registerUser, token, loginStatus, unRegisterUser }}>{props.children}</UserContext.Provider>;
 };
 
 export { UserContext, UserContextProvider };
