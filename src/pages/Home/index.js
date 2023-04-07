76e6b20b1582495a18a890d16b321112d3cb72f6
sivakusi12 <sivakusi12@gmail.com>
2023-04-07 22:43:14 +0530

    Added more features

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,43 @@
-import { Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Container, Grid, Typography } from '@mui/material';
 import { Suspense, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Transition from '../../components/System/Transition';
 import { RoutesContext } from '../../context/RoutesContext';
 
 function Home() {
-  const { homeRoutes } = useContext(RoutesContext);
+	const { homeRoutes } = useContext(RoutesContext);
 
-  return (
-    <>
-      <Suspense fallback='Loading...'>
-        <Transition>
-          <Container maxWidth={false} sx={{ mt: 2, maxWidth: '85%' }}>
-            {homeRoutes && (
-              <Grid container justifyContent='center' spacing={2} sx={{ mt: 2, p: 1, flexGrow: 1 }}>
-                {homeRoutes.map(elem => (
-                  <Grid item key={elem.id} xs={12} sm={12} md={4} lg={3} xl={3} variant='button' underline='none' component={Link} to={elem.route}>
-                    <Card sx={{ height: 100 }} variant='elevation' elevation={16}>
-                      {/** neon blue: 0FF0FC */}
-                      {/* <CardActionArea> */}
-                      <CardContent>
-                        <Typography sx={{ fontSize: 14, textTransform: 'uppercase' }} color='text.primary' gutterBottom>
-                          {elem.label}
-                        </Typography>
-                      </CardContent>
-                      {/* </CardActionArea> */}
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            )}
-          </Container>
-        </Transition>
-      </Suspense>
-    </>
-  );
+	return (
+		<>
+			<Suspense fallback='Loading...'>
+				<Transition>
+					<Container maxWidth={false} sx={{ mt: 2, maxWidth: '85%' }}>
+						{homeRoutes && (
+							<Grid container justifyContent='center' spacing={2} sx={{ mt: 2, p: 1, flexGrow: 1 }}>
+								{homeRoutes.map(elem => (
+									<Grid item key={elem.id} xs={12} sm={12} md={4} lg={3} xl={3} variant='button' underline='none' component={Link} to={elem.route}>
+										<Card sx={{ minHeight: 80, p: 1, pl: 2, pt: 2, height: 100 }} color='secondary' variant='elevation' elevation={20}>
+											<CardHeader
+												sx={{ p: 0 }}
+												title={elem.label}
+												titleTypographyProps={{ variant: 'h6', sx: { fontSize: '18px', mb: 0, pb: 0 }, gutterBottom: false, color: 'text.primary' }}
+												subheader={`${elem.description}`}
+												subheaderTypographyProps={{
+													sx: { fontSize: '11px', mb: 0, pb: 0 },
+													gutterBottom: true,
+													color: 'text.disabled',
+												}}
+											/>
+										</Card>
+									</Grid>
+								))}
+							</Grid>
+						)}
+					</Container>
+				</Transition>
+			</Suspense>
+		</>
+	);
 }
 
 export default Home;
